73646af2ac1dacb36ad73a9c483ce3cf1b97a6a2
Atishay25 <atishayjain2552@gmail.com>
2022-09-16 00:15:11 +0530

    Codeforces Initial

---
+++ codeforces/team.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, p1, p2, p3;
+	cin >> n;
+	int solve[n];
+	int solution = 0;
+	for(int i = 0; i < n; i++){
+	        cin >> p1 >> p2 >> p3;
+		solve[i] = p1 + p2 + p3;
+	}
+	for(int i = 0; i < n; i++){
+		if(solve[i] >= 2) solution++; 
+	}
+	cout << solution << endl;
+}	
