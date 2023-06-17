6cde3d7da4dc87279885642d34591038a5708577
Atishay25 <atishayjain2552@gmail.com>
2023-06-17 22:29:08 +0530

    cses collecting numbers 1

--- week3_4-soc/README.md
+++ week3_4-soc/README.md
@@ -1,12 +1,10 @@
 # Week 3 | Data Structures, Sorting and Searching
 
-2. CSES Collecting Numbers (here, we have to take numbers in sequence
-a,a+1,a+2... sequences where difference of consecutive terms is not 1 is invalid)
-3. CSES Collecting Numbers 2
-4. Codeforces 1817A
-6.  Codeforces 1810D
-7.  CSES Sliding Cost (If this seems too tough, try CSES Sliding Median first)
-8.  CSES Subarray Distinct Values
+1. CSES Collecting Numbers 2
+2. Codeforces 1817A
+3.  Codeforces 1810D
+4.  CSES Sliding Cost (If this seems too tough, try CSES Sliding Median first)
+5.  CSES Subarray Distinct Values
 
 # Week 4 | Number Theory, Sieve, Greedy and DP
 1. Codeforces 1819A
---
+++ week3_4-soc/cses_collecting_numbers.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+//  
+// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef long double ld;
+typedef uint64_t ull;
+typedef int64_t ll;
+typedef vector<int> vi;
+typedef vector<int64_t> vll;
+
+typedef pair<int,int> pii;
+typedef pair<int64_t,int64_t> pll;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpll;
+
+#define pb push_back
+#define pob pop_back()
+#define sz size()
+#define ff first
+#define ss second
+#define mp make_pair
+#define PI 3.14159265359
+#define INF 1500000000000000000
+#define NINF -1500000000000000000
+#define MOD 1000000007          // 10^9 + 7
+#define loop0(i,n) for(ll i=0;i<n;i++)
+#define loop1(i,n) for(ll i=1;i<=n;i++)
+#define o0(a) cout<<a<<" "
+#define o1(a) cout<<a<<"\n"
+#define o2(a, b) cout<<a<<" "<<b<<"\n"
+
+template<typename T>
+void print_array(T *a, ll sz){
+    loop0(i,sz){
+        o0(a[i]);
+    }
+    cout << "\n";
+}
+
+template<typename T>
+void print_vector(vector<T> a){
+    for(auto it:a){
+        o0(it);
+    }
+    cout << "\n";
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin>>n;
+    vll v(n);
+    map<ll,ll> index;
+    loop0(i,n){
+        cin >> v[i];
+        index[v[i]] = i;
+    }
+    ll ans = 0;
+    ll num = 1;
+    while(num <= n){
+        while(index[num+1] > index[num]){
+            num++;
+        }
+        ans++;
+        num++;
+    }
+    o1(ans);
+    
+}
+
+
